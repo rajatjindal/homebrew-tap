bbebad87049a5c9fa652ee12dc786ad284625fc7
Rajat Jindal <rajatjindal83@gmail.com>
2024-01-15 12:35:18 +0530

    update to version 0.0.46
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- Formula/modify-secret.rb
+++ Formula/modify-secret.rb
@@ -1,26 +1,26 @@
 class ModifySecret < Formula
   desc "a kubectl plugin, that allows user to modify the secret without having to worry about doing base64 encoding/decoding"
   homepage "https://github.com/rajatjindal/kubectl-modify-secret"
-  version "0.0.45"
+  version "0.0.46"
 
   if OS.mac? && Hardware::CPU.intel?
 		url "https://github.com/rajatjindal/kubectl-modify-secret/releases/download/v#{version}/kubectl-modify-secret_v#{version}_darwin_amd64.tar.gz"
-    sha256 "4994aacf2aa2a6d8029dba7d80b619c466c9a19ebf5f5f86088ce6b0fa222326"
+    sha256 "92ae5624935107ba354b5d245d305845ff7ef07a03f7088cea45ed7c33eed319"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/rajatjindal/kubectl-modify-secret/releases/download/v#{version}/kubectl-modify-secret_v#{version}_darwin_arm64.tar.gz"
-    sha256 "46c0162212fa57d91a2866c8b8a42658f60cdf38baa143c10b44340ddadb7cc5"
+    sha256 "7ce141459ded189bdbf2aeb498cdc1c07b5bcc5a29c7d9ef6fdb479bffba4075"
   end
 
   if OS.linux? && Hardware::CPU.intel?
 		url "https://github.com/rajatjindal/kubectl-modify-secret/releases/download/v#{version}/kubectl-modify-secret_v#{version}_linux_amd64.tar.gz"
-    sha256 "3f3ede2d9b4d4baee818019619642571b254092954800ee31f76ba12d88a4bb7"
+    sha256 "cf60808b0f4edfdc0a53d179b0e86a61df4c0df1a3ff50f9f4931d4bd551ee7d"
   end
 
   if OS.linux? && Hardware::CPU.arm?
 		url "https://github.com/rajatjindal/kubectl-modify-secret/releases/download/v#{version}/kubectl-modify-secret_v#{version}_linux_arm64.tar.gz"
-    sha256 "7f26953a6d8e74613e25da9e2d9d6f070ef3e0ad7ec45e6c5c3acc04ceb1d67f"
+    sha256 "4a7a3badb8867479133651eb3ba08dea9925b55344f7d38c1359e7d61c6333b4"
   end
 
   def install
